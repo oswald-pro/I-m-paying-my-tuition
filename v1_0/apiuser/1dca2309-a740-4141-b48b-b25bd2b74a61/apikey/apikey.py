b2ad7b8c47ff6466debdbfc1d865ae10943e0b55
oswald-pro <oswaldmboussa1@gmail.com>
2020-03-27 15:19:08 +0000

    update

--- v1_0/apiuser/1dca2309-a740-4141-b48b-b25bd2b74a61/apikey/apikey.py
+++ v1_0/apiuser/1dca2309-a740-4141-b48b-b25bd2b74a61/apikey/apikey.py
@@ -7,7 +7,7 @@ headers = {
 params = urllib.urlencode({
 })
 body = json.dumps({
-  "providerCallbackHost": "https://github.com/oswald-pro/I-m-paying-my-tuition/blob/master/v1_0/apiuser/62d73086-8f0e-4117-af0b-413fb0c9f975/apikey/apikey.py" })
+  "providerCallbackHost": "https://github.com/oswald-pro/I-m-paying-my-tuition/blob/master/v1_0/apiuser/1dca2309-a740-4141-b48b-b25bd2b74a61/apikey/apikey.py" })
 try:
     conn = httplib.HTTPSConnection('sandbox.momodeveloper.mtn.com')
     conn.request("POST", "/v1_0/apiuser/1dca2309-a740-4141-b48b-b25bd2b74a61/apikey?%s" % params, body, headers)
